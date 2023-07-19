706a6742c30b5dc6a763c949a91555a2973e7697
lbartosik <lbartosik@pepco.eu>
2023-07-19 13:54:24 +0200

    "First of all"

--- TaskMngr.java
+++ TaskMngr.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Main
+public class TaskMngr
 {
+
     private List<String> Tasks;
     
     
@@ -33,49 +34,42 @@ public class Main
         
     }
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		Main newObj=new Main();
-		
-		newObj.Init();
+		TaskMngr newObj = new TaskMngr();
+
+//		newObj.Init();
 		newObj.populateArray();
 		newObj.viewAll();
-		
-		String name="Jason";
-		
-		
-		while (true){
-		    System.out.println("Aby dodac zadanie wybierz 1.");
-		    System.out.println("Aby usunąć zadanie wybierz 2.");
-		    System.out.println("Aby wyświetlić zadania wybierz 3.");
-		    int choice=scanner.nextInt();
-		    
-		    switch(choice){
-		        
-		        
-		        
-		        case 1:
-		            
-		            System.out.println("Wpisz zadanie");
-		            
-		            String noweZadanie=scanner.nextLine();
-		            
-		            newObj.addTasks(noweZadanie);
-		            
-		            
-		            
-		            
-		            
-		    }
-		    
-		    
-		    
-		}
-		
-		
-		
 
-		
+		String name = "Jason";
+
+
+		while (true) {
+			System.out.println("Aby dodac zadanie wybierz 1.");
+			System.out.println("Aby usunąć zadanie wybierz 2.");
+			System.out.println("Aby wyświetlić zadania wybierz 3.");
+			int choice = scanner.nextInt();
+
+			switch (choice) {
+
+
+				case 1:
+
+					System.out.println("Wpisz zadanie");
+
+					String noweZadanie = scanner.next();
+
+					newObj.addTasks(noweZadanie);
+
+
+			}
+
+
+		}
+
+
+
 		
 	}
 }
