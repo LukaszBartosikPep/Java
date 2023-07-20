45b2e1d7574b4d8c7f9a8c9c093c5e289e857c65
lbartosik <lbartosik@pepco.eu>
2023-07-20 12:30:02 +0200

    "First of all"

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        TaskMngr newObj=new TaskMngr();
+
+        newObj.viewAll();
+    }
+}
--- TaskMngr.java
+++ TaskMngr.java
@@ -5,34 +5,58 @@ import java.util.ArrayList;
 public class TaskMngr
 {
 
-    private List<String> Tasks;
+   private ArrayList<String> Tasks=new ArrayList<>();
     
     
     
-    public void Init(){
+
         
-        Tasks=new ArrayList<>();
-    }
+
+
     public void populateArray(){
-        
-        
-        
-        Tasks.add("First");
-        
-    }
+
+
+		Tasks.add("First");
+		Tasks.add("Second");
+		Tasks.add("Third");
+
+	}
     public void addTasks(String noweZadanie){
-        
+		int size=Tasks.size();
         Tasks.add(noweZadanie);
-        
+
         
     }
     public void viewAll(){
-        System.out.println(Tasks);
+
+//		int size=Tasks.size();
+//		for(int i=1; i<=size;i++){
+//			int
+//			String j =toString();
+//			Tasks.add(i,j);
+////
+//		}
+		System.out.println(Tasks);
+
         
     }
-    public void del(){
-        
+    public void del(int numb){
+		int size=Tasks.size();
+//		System.out.println(size);
+//		for(int i=1; i<=size;i++){
+//
+//			System.out.println(i);
+//		}
+		Tasks.remove(numb-1);
     }
+	public void mark(int sign){
+		String modifyTask=Tasks.get(sign-1)+ "*";
+		Tasks.set(sign-1, modifyTask);
+
+
+
+	}
+
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
@@ -42,13 +66,14 @@ public class TaskMngr
 		newObj.populateArray();
 		newObj.viewAll();
 
-		String name = "Jason";
+
 
 
 		while (true) {
 			System.out.println("Aby dodac zadanie wybierz 1.");
 			System.out.println("Aby usunąć zadanie wybierz 2.");
-			System.out.println("Aby wyświetlić zadania wybierz 3.");
+			System.out.println("Aby zaznaczyć zadanie wybierz 3.");
+
 			int choice = scanner.nextInt();
 
 			switch (choice) {
@@ -62,8 +87,24 @@ public class TaskMngr
 
 					newObj.addTasks(noweZadanie);
 
+				case 2:
+					 System.out.println("Które zadanie chcesz usunąć, wpisz numer");
+					 newObj.viewAll();
+
+					 //Wyświetl numery zadań i po wpisaniu numeru usun zadanie
+					 int numb = scanner.nextInt();
+					 newObj.del(numb);
+					 newObj.viewAll();
+				case 3:
+					System.out.println("Które zadanie chcesz zaznaczyć?");
+					int sign= scanner.nextInt();
+					newObj.mark(sign);
+					newObj.viewAll();
+
+
 
 			}
+
 
 
 		}
