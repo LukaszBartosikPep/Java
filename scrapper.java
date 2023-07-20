4774b5c1310f6cea74f3064035f437348621db86
lbartosik <lbartosik@pepco.eu>
2023-07-20 13:05:21 +0200

    "First of all"

--- scrapper.java
+++ scrapper.java
@@ -1,2 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class scrapper {
+    public static void main(String[] args) {
+        // Set the path to the Internet Explorer driver executable
+        System.setProperty("webdriver.ie.driver", "C:/Users/lbartosik/OneDrive - PEPCO/Pulpit/IEDriverServer.exe");
+
+        // Create a new instance of the InternetExplorerDriver
+        WebDriver driver = new InternetExplorerDriver();
+
+        // Navigate to a website
+        driver.get("https://www.google.com");
+
+        // Get the title of the page and print it
+        String pageTitle = driver.getTitle();
+        System.out.println("Page title: " + pageTitle);
+
+        // Close the browser
+        driver.quit();
+    }
 }
